d3ae3765f19bc5d13d884ef51c81f1c2e6fe73d7
Stiliyan Valchev <stil.vulchev@gmail.com>
2024-07-09 12:28:31 +0300

    Add files via upload

---
+++ data_operations_in_django_with_queries_exercise/caller.py
@@ -0,0 +1,257 @@
+import os
+import django
+from django.db.models import QuerySet, F
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+from main_app.models import Pet, Artifact, Location, Car, Task, HotelRoom, Character
+
+
+def create_pet(name: str, species: str) -> str:
+    pet = Pet.objects.create(
+        name=name,
+        species=species,
+    )
+
+    return f"{pet.name} is a very cute {pet.species}!"
+
+
+# print(create_pet('Buddy', 'Dog'))
+# print(create_pet('Whiskers', 'Cat'))
+# print(create_pet('Rocky', 'Hamster'))
+
+
+def create_artifact(name: str, origin: str, age: int, description: str, is_magical: bool) -> str:
+    artifact = Artifact.objects.create(
+        name=name,
+        origin=origin,
+        age=age,
+        description=description,
+        is_magical=is_magical,
+    )
+
+    return f"The artifact {artifact.name} is {artifact.age} years old!"
+
+
+def rename_artifact(artifact: Artifact, new_name: str) -> None:
+    if artifact.is_magical and artifact.age > 250:
+        artifact.name = new_name
+        artifact.save()
+
+
+def delete_all_artifacts() -> None:
+    Artifact.objects.all().delete()
+
+
+# delete_all_artifacts()
+# print(create_artifact('Ancient Sword', 'Lost Kingdom', 500, 'A legendary sword with a rich history', True))
+# artifact_object = Artifact.objects.get(name='Ancient Sword')
+# rename_artifact(artifact_object, 'Ancient Shield')
+# print(artifact_object.name)
+
+
+def show_all_locations() -> str:
+    locations = Location.objects.order_by('-id')
+
+    return '\n'.join(str(location) for location in locations)
+
+
+def new_capital() -> None:
+    location = Location.objects.first()
+    location.is_capital = True
+    location.save()
+
+
+def get_capitals() -> QuerySet:
+    return Location.objects.filter(is_capital=True).values('name')
+
+
+def delete_first_location() -> None:
+    Location.objects.first().delete()
+
+
+# print(show_all_locations())
+# print(new_capital())
+# print(get_capitals())
+
+
+def apply_discount() -> None:
+    cars = Car.objects.all()
+
+    for car in cars:
+        discount_percent = sum(int(ch) for ch in str(car.year)) / 100
+        discount = float(car.price) * discount_percent
+        car.price_with_discount = float(car.price) - discount
+        car.save()
+
+
+def get_recent_cars() -> QuerySet:
+    return Car.objects.filter(year__gt=2020).values('model', 'price_with_discount')
+
+
+def delete_last_car() -> None:
+    Car.objects.last().delete()
+
+
+# apply_discount()
+# print(get_recent_cars())
+
+
+def show_unfinished_tasks() -> str:
+    tasks = Task.objects.filter(is_finished=False)
+
+    return '\n'.join(str(t) for t in tasks)
+
+
+def complete_odd_tasks() -> None:
+    tasks = Task.objects.all()
+
+    for task in tasks:
+        if task.id % 2 == 1:
+            task.is_finished = True
+
+    Task.objects.bulk_update(tasks, ['is_finished'])
+
+
+def encode_and_replace(text: str, task_title: str) -> None:
+    decoded = ''.join(chr(ord(ch) - 3) for ch in text)
+    Task.objects.filter(title=task_title).update(description=decoded)
+
+#
+# encode_and_replace("Zdvk#wkh#glvkhv$", "Simple Task")
+
+
+def get_deluxe_rooms() -> str:
+    deluxe_rooms = HotelRoom.objects.filter(room_type="Deluxe")
+    even_deluxe_rooms = [str(room) for room in deluxe_rooms if room.id % 2 == 0]
+
+    return '\n'.join(even_deluxe_rooms)
+
+
+def increase_room_capacity() -> None:
+    rooms = HotelRoom.objects.all().order_by('id')
+
+    previous_room_capacity = None
+
+    for room in rooms:
+        if not room.is_reserved:
+            continue
+
+        if previous_room_capacity is not None:
+            room.capacity += previous_room_capacity
+        else:
+            room.capacity += room.id
+
+        previous_room_capacity = room.capacity
+
+    HotelRoom.objects.bulk_update(rooms, ['capacity'])
+
+
+def reserve_first_room() -> None:
+    room = HotelRoom.objects.first()
+    room.is_reserved = True
+    room.save()
+
+
+def delete_last_room() -> None:
+    room = HotelRoom.objects.filter(is_reserved=False).last()
+    room.delete()
+
+
+# print(get_deluxe_rooms())
+# reserve_first_room()
+# print(HotelRoom.objects.get(room_number=401).is_reserved)
+
+
+def update_characters() -> None:
+    Character.objects.filter(class_name='Mage').update(
+        lavel=F('level') + 3,
+        intelligence=F('intelligence') - 7,
+    )
+
+    Character.objects.filter(class_name='Warrior').update(
+        hit_points=F('hit_points') / 2,
+        dexterity=F('dexterity') + 4,
+    )
+
+    Character.objects.filter(class_name__in=['Assassin', 'Scout']).update(
+        inventory='Inventory is empty',
+    )
+
+
+def fuse_characters(first_character: Character, second_character: Character) -> None:
+    fuse_name = first_character.name + ' ' + second_character.name
+    class_name = 'Fusion'
+    level = (first_character.level + second_character.level) // 2
+    strength = (first_character.strength + second_character.strength) * 1.2
+    dexterity = (first_character.dexterity + second_character.dexterity) * 1.4
+    intelligence = (first_character.intelligence + second_character.intelligence) * 1.5
+    hit_points = first_character.hit_points + second_character.hit_points
+
+    if first_character.class_name in ["Mage", "Scout"]:
+        inventory = "Bow of the Elven Lords, Amulet of Eternal Wisdom"
+    else:
+        inventory = "Dragon Scale Armor, Excalibur"
+
+    Character.objects.create(name=fuse_name,
+                             class_name=class_name,
+                             level=level,
+                             strength=strength,
+                             dexterity=dexterity,
+                             intelligence=intelligence,
+                             hit_points=hit_points,
+                             inventory=inventory)
+
+    first_character.delete()
+    second_character.delete()
+
+
+def grand_dexterity() -> None:
+    Character.objects.update(dexterity=30)
+
+
+def grand_intelligence() -> None:
+    Character.objects.update(intelligence=40)
+
+
+def grand_strength() -> None:
+    Character.objects.update(strength=50)
+
+
+def delete_characters() -> None:
+    Character.objects.filter(inventory="The inventory is empty").delete()
+
+
+# character1 = Character.objects.create(
+#     name='Gandalf',
+#     class_name='Mage',
+#     level=10,
+#     strength=15,
+#     dexterity=20,
+#     intelligence=25,
+#     hit_points=100,
+#     inventory='Staff of Magic, Spellbook',
+# )
+#
+# character2 = Character.objects.create(
+#     name='Hector',
+#     class_name='Warrior',
+#     level=12,
+#     strength=30,
+#     dexterity=15,
+#     intelligence=10,
+#     hit_points=150,
+#     inventory='Sword of Troy, Shield of Protection',
+# )
+#
+# fuse_characters(character1, character2)
+# fusion = Character.objects.filter(class_name='Fusion').get()
+#
+# print(fusion.name)
+# print(fusion.class_name)
+# print(fusion.level)
+# print(fusion.intelligence)
+# print(fusion.inventory)
